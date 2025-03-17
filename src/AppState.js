5400268405ebaca5523d43515edbac26ed820c12
d-hooper <dallinhoop3@gmail.com>
2025-03-17 14:30:33 -0600

    added upgrades to app state

--- src/AppState.js
+++ src/AppState.js
@@ -7,6 +7,38 @@ export const AppState = reactive({
   /** @type {import('./models/Account.js').Account} user info from the database*/
   account: null,
   cheese: 0,
-  clickAmount: 1
+  clickUpgrades: [
+    {
+      id: 1,
+      name: 'Centaur',
+      price: 5,
+      quantity: 0,
+      multiplier: 1 
+    },
+    {
+      id: 2,
+      name: 'Cheese Chariot',
+      price: 50,
+      quantity: 0,
+      multiplier: 1 
+    },
+  ],
+  autoUpgrades: [
+    {
+      id: 1,
+      name: 'Boon from Olympus',
+      price: 1000,
+      quantity: 0,
+      multiplier: 1 
+    },
+    {
+      id: 2,
+      name: 'The Hands of Fate',
+      price: 10000,
+      quantity: 0,
+      multiplier: 1 
+    },
+
+  ]
 })
 
